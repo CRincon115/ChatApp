d49b6e2071e03b091be98a5ad87f42af71fdf581
CRincon115 <solracrincon115@gmail.com>
2023-05-22 19:48:26 -0700

    Segundo commit

--- app/src/main/java/com/example/chatalpha/MainActivity.java
+++ app/src/main/java/com/example/chatalpha/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.chatalpha;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.example.chatalpha.activities.LoginActivity;
 import com.example.chatalpha.activities.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Switch switcher;
+
+    boolean nightMODE;
+
+    SharedPreferences sharedPreferences;
+
+    SharedPreferences.Editor editor;
     private Button loginButton;
     private Button registerButton;
 
@@ -18,6 +31,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.hide();
+//        }
+
+        getSupportActionBar().hide();
+
+        switcher = findViewById(R.id.switcher);
+
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        nightMODE = sharedPreferences.getBoolean("night", true);
+
+        if (nightMODE){
+            switcher.setChecked(false);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
+        switcher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (nightMODE){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("night", false);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("night", true);
+                }
+                editor.apply();
+            }
+        });
 
         loginButton = findViewById(R.id.loginButton);
         registerButton = findViewById(R.id.registerButton);
